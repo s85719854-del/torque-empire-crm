abed9593e8437cb40148cf8c79b7c2e586051201
s85719854-del <s85719854@gmail.com>
2025-09-16 23:06:33 +0200

    Create firebaseConfig.js
    chore: added firebase config utils

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+// Firebase Config for Torque Empire Ai_Pro CRM
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
